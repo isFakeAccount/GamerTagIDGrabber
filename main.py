c8054cb136b38e61d322742c82661ac61e315069
isFakeAccount <trevorphillips@gmx.us>
2025-05-12 19:49:50 -0400

    Add check to make sure correct DISCORD_TOKEN is set
    Add check to make sure correct DISCORD_TOKEN is set

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import hikari
 from dotenv import load_dotenv
 
 load_dotenv()
-bot = hikari.GatewayBot(getenv("DISCORD_TOKEN", "DISCORD_TOKEN"))
+discord_token = getenv("DISCORD_TOKEN")
+if discord_token is None:
+    msg = "DISCORD_TOKEN environment variable not set. " \
+    "Please make sure you have .env file in project root directory and contains the right environment variables."
+    raise ValueError(msg)
+bot = hikari.GatewayBot(discord_token)
 client = crescent.Client(bot)
 client.plugins.load("plugins.gamertag_commands")
 
