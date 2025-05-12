a78cd2758b5eef47b56ced5102e5119f78269186
isFakeAccount <trevorphillips@gmx.us>
2025-05-12 19:51:20 -0400

    Move code to main

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import crescent
 import hikari
 from dotenv import load_dotenv
 
-load_dotenv()
-discord_token = getenv("DISCORD_TOKEN")
-if discord_token is None:
-    msg = "DISCORD_TOKEN environment variable not set. " \
-    "Please make sure you have .env file in project root directory and contains the right environment variables."
-    raise ValueError(msg)
-bot = hikari.GatewayBot(discord_token)
-client = crescent.Client(bot)
-client.plugins.load("plugins.gamertag_commands")
-
 
 def main() -> None:
+    load_dotenv()
+    discord_token = getenv("DISCORD_TOKEN")
+    if discord_token is None:
+        msg = (
+            "DISCORD_TOKEN environment variable not set. "
+            "Please make sure you have .env file in project root directory and contains the right environment variables."
+        )
+        raise ValueError(msg)
+
+    bot = hikari.GatewayBot(discord_token)
+    client = crescent.Client(bot)
+    client.plugins.load("plugins.gamertag_commands")
     bot.run()
 
 
